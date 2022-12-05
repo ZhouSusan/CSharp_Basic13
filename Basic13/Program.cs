0e91673a60af8e36a7f13a6d3c79db2811cc1b15
ZhouSusan <szhou089@gmail.com>
2022-12-04 19:53:43 -0800

    added printOddNum1To255() that prints odd number between 1 to 255 in Program.cs

--- Basic13/Program.cs
+++ Basic13/Program.cs
@@ -2,6 +2,7 @@
 {
     public class Program
     {
+        //Method that print all integer from 1 to 255
         static void print1To255 ()
         {
             for (int i = 1; i <= 255; i++)
@@ -9,9 +10,18 @@
                 Console.WriteLine(i);
             }
         }
+
+        static void printOddNum1To255()
+        {
+            for (int j = 1; j <= 255; j+=2)
+            {
+                Console.WriteLine(j);
+            }
+        }
         public static void Main()
         {
             print1To255();
+            printOddNum1To255();
         }
     }
 }
